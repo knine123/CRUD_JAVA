313fa6fc1c1715fe214d37ad1fa4cdcd204ec835
Kim Seongji <22000097@handong.ac.kr>
2023-09-03 15:40:18 +0900

    가독성 증진

--- src/main/java/CRUD/WordCRUD.java
+++ src/main/java/CRUD/WordCRUD.java
@@ -13,7 +13,7 @@ public class WordCRUD implements ICRUD{
     }
     @Override
     public Object add() {
-        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        System.out.print("\n=> 난이도(1,2,3) & 새 단어 입력 : ");
         int level = s.nextInt();
         String word = s.nextLine();//버퍼를 없애기 위하여
         System.out.print("뜻 입력 : ");
@@ -25,7 +25,7 @@ public class WordCRUD implements ICRUD{
     public void addWord(){
         Word one = (Word)add();
         list.add(one);
-        System.out.println("새 단어가 단어장에 추가되었습니다.");
+        System.out.println("\n새 단어가 단어장에 추가되었습니다!!!\n");
 
     }
 
--- src/main/java/CRUD/WordManager.java
+++ src/main/java/CRUD/WordManager.java
@@ -10,11 +10,11 @@ public class WordManager {
         wordCRUD = new WordCRUD(s);
     }
     public int selectMenu(){
-        System.out.println("*** 영단어 마스터 ***");
         System.out.print("*********************\n1. 모든 단어 보기\n2. 수준별 단어 보기\n3. 단어 검색\n4. 단어 추가\n5. 단어 수정\n6. 단어 삭제\n7. 파일 저장\n0. 나가기\n*********************\n=> 원하는 메뉴는? ");
         return s.nextInt();
     }
     public void start(){
+        System.out.println("*** 영단어 마스터 ***\n");
         while(true) {
             int menu = selectMenu();
             if(menu==0) {//나가기
