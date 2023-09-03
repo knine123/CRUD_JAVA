93a4c42dfa0f6a7c689a38b01dc3fe1c4ef8ab58
Kim Seongji <22000097@handong.ac.kr>
2023-09-03 15:50:59 +0900

    첫번째 알파벳 대문자로 변환

--- src/main/java/CRUD/WordCRUD.java
+++ src/main/java/CRUD/WordCRUD.java
@@ -1,6 +1,7 @@
 package CRUD;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class WordCRUD implements ICRUD{
@@ -16,6 +17,8 @@ public class WordCRUD implements ICRUD{
         System.out.print("\n=> 난이도(1,2,3) & 새 단어 입력 : ");
         int level = s.nextInt();
         String word = s.nextLine();//버퍼를 없애기 위하여
+        word=word.substring(1,2).toUpperCase()+ word.substring(2);
+        //작성시 띄어쓰기까지 포함시켜서 첫번째 부분은 공백이다. 그래서 0,1이 아닌 1,2가 된다.
         System.out.print("뜻 입력 : ");
         String meaning = s.nextLine();//공백까지 모두 입력받기 위해서
 
